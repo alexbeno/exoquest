afafd8cc9334ad599fdb1c58f9f7d2370ed41d6e
Célia Dagorne <celia.dagorne@hotmail.fr>
2017-03-29 10:13:37 +0200

    :art: 1st form style

--- views/pages/exoplanets.php
+++ views/pages/exoplanets.php
@@ -16,27 +16,41 @@ $nb_pla = (int)$_POST['pl-sys'];
 ?>
 
 <form method="post">
-	<label for="disc-year">Discovery year: </label>
-	<input type="number" name="disc-year"><br>
-	<label for="up-temp">Temperature upper: </label>
-	<input type="number" name="up-temp">K<br>
-	<label for="bel-temp">Temperature below: </label>
-	<input type="number" name="bel-temp">K<br>
-	<label for="disc-met">Discovery method: </label>
-	<select name="disc-met">
-		<option value="Radial Velocity">Radial Velocity</option>
-		<option value="Microlensing">Microlensing</option>
-		<option value="Transit">Transit</option>
-		<option value="Imaging">Imaging</option>
-	</select><br>
-	<label for="up-mass">Mass upper: </label>
-	<input type="number" name="up-mass">M☉<br>
-	<label for="bel-mass">Mass below: </label>
-	<input type="number" name="bel-mass">K<br>
-	<label for="pl-sys">Planets in the system: </label>
-	<input type="number" name="pl-sys">
+	<div class="group">
+		<label for="disc-year">Discovery year: </label>
+		<input type="number" name="disc-year">
+	</div>
+	<div class="group">
+		<label for="up-temp">Temperature upper: </label>
+		<input type="number" name="up-temp">K
+	</div>
+	<div class="group">
+		<label for="bel-temp">Temperature below: </label>
+		<input type="number" name="bel-temp">K
+	</div>
+	<div class="group">
+		<label for="disc-met">Discovery method: </label>
+		<select name="disc-met">
+			<option value="Radial Velocity">Radial Velocity</option>
+			<option value="Microlensing">Microlensing</option>
+			<option value="Transit">Transit</option>
+			<option value="Imaging">Imaging</option>
+		</select>
+	</div>
+	<div class="group">
+		<label for="up-mass">Mass upper: </label>
+		<input type="number" name="up-mass">M☉
+	</div>
+	<div class="group">
+		<label for="bel-mass">Mass below: </label>
+		<input type="number" name="bel-mass">K
+	</div>
+	<div class="group">
+		<label for="pl-sys">Planets in the system: </label>
+		<input type="number" name="pl-sys">
+	</div>
 	<input type="submit">
-	</form>
+</form>
 
 <div class="info-container">
 	<div class="info-names">
@@ -56,87 +70,87 @@ $nb_pla = (int)$_POST['pl-sys'];
 	<div class="info-datas">
 		<?php foreach($result as $planet):
 			// constuct the perfect request
-			if(!empty($_POST['disc-year']))
+		if(!empty($_POST['disc-year']))
+		{
+			$cond1 = ($planet->pl_disc === $year_data);
+			$req = $cond1;
+		}
+		if(!empty($_POST['up-temp']))
+		{
+			$cond2 = ($planet->st_teff >= $temp_data);
+			if(empty($_POST['disc-year']))
 			{
-				$cond1 = ($planet->pl_disc === $year_data);
-				$req = $cond1;
+				$req = $cond2;
 			}
-			if(!empty($_POST['up-temp']))
+			else
 			{
-				$cond2 = ($planet->st_teff >= $temp_data);
-				if(empty($_POST['disc-year']))
-				{
-					$req = $cond2;
-				}
-				else
-				{
-					$req = $req && $cond2;
-				}
+				$req = $req && $cond2;
 			}
-			if(!empty($_POST['bel-temp']))
+		}
+		if(!empty($_POST['bel-temp']))
+		{
+			$cond3 = ($planet->st_teff <= $temp_be_data);
+			if(empty($_POST['disc-year']) && empty($_POST['disc-year']))
 			{
-				$cond3 = ($planet->st_teff <= $temp_be_data);
-				if(empty($_POST['disc-year']) && empty($_POST['disc-year']))
-				{
-					$req = $cond3;
-				}
-				else
-				{
-					$req = $req && $cond3;
-				}
+				$req = $cond3;
 			}
-			if(!empty($_POST['disc-met']))
+			else
 			{
-				$cond4 = ($planet->pl_discmethod <= $disc_met);
-				if(empty($_POST['disc-year']) && empty($_POST['up-temp']) && empty($_POST['bel-temp']))
-				{
-					$req = $cond4;
-				}
-				else
-				{
-					$req = $req && $cond4;
-				}
+				$req = $req && $cond3;
 			}
-			if(!empty($_POST['up-mass']))
+		}
+		if(!empty($_POST['disc-met']))
+		{
+			$cond4 = ($planet->pl_discmethod <= $disc_met);
+			if(empty($_POST['disc-year']) && empty($_POST['up-temp']) && empty($_POST['bel-temp']))
 			{
-				$cond5 = ($planet->st_mass >= $up_mass);
-				if(empty($_POST['disc-year']) && empty($_POST['up-temp']) && empty($_POST['bel-temp']) && empty($_POST['disc-met']))
-				{
-					$req = $cond5;
-				}
-				else
-				{
-					$req = $req && $cond5;
-				}
+				$req = $cond4;
 			}
-			if(!empty($_POST['bel-mass']))
+			else
 			{
-				$cond6 = ($planet->st_mass <= $be_mass);
-				if(empty($_POST['disc-year']) && empty($_POST['up-temp']) && empty($_POST['bel-temp']) && empty($_POST['disc-met']) && empty($_POST['up-mass']))
-				{
-					$req = $cond6;
-				}
-				else
-				{
-					$req = $req && $cond6;
-				}
+				$req = $req && $cond4;
 			}
-			if(!empty($_POST['pl-sys']))
+		}
+		if(!empty($_POST['up-mass']))
+		{
+			$cond5 = ($planet->st_mass >= $up_mass);
+			if(empty($_POST['disc-year']) && empty($_POST['up-temp']) && empty($_POST['bel-temp']) && empty($_POST['disc-met']))
 			{
-				$cond7 = ($planet->pl_pnum === $nb_pla);
-				if(empty($_POST['disc-year']) && empty($_POST['up-temp']) && empty($_POST['bel-temp']) && empty($_POST['disc-met']) && empty($_POST['up-mass']) && empty($_POST['bel-mass']))
-				{
-					$req = $cond7;
-				}
-				else
-				{
-					$req = $req && $cond7;
-				}
+				$req = $cond5;
 			}
+			else
+			{
+				$req = $req && $cond5;
+			}
+		}
+		if(!empty($_POST['bel-mass']))
+		{
+			$cond6 = ($planet->st_mass <= $be_mass);
+			if(empty($_POST['disc-year']) && empty($_POST['up-temp']) && empty($_POST['bel-temp']) && empty($_POST['disc-met']) && empty($_POST['up-mass']))
+			{
+				$req = $cond6;
+			}
+			else
+			{
+				$req = $req && $cond6;
+			}
+		}
+		if(!empty($_POST['pl-sys']))
+		{
+			$cond7 = ($planet->pl_pnum === $nb_pla);
+			if(empty($_POST['disc-year']) && empty($_POST['up-temp']) && empty($_POST['bel-temp']) && empty($_POST['disc-met']) && empty($_POST['up-mass']) && empty($_POST['bel-mass']))
+			{
+				$req = $cond7;
+			}
+			else
+			{
+				$req = $req && $cond7;
+			}
+		}
 		?>
 		<?php if($req)
 		{
-		?>
+			?>
 			<div class="data-row">
 				<table>
 					<tbody>
@@ -151,13 +165,13 @@ $nb_pla = (int)$_POST['pl-sys'];
 					</tbody>
 				</table>
 			</div>
-		<?php
+			<?php
 		}
 		else
 		{
 
 		}
 		?>
-		<?php endforeach ?>
-	</div>
+	<?php endforeach ?>
+</div>
 </div>
